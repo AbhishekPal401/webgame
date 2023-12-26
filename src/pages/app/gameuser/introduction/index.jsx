32aa9f22d9c5ce430d2b767a34e56d71d7b15ae9
abhishek.kp <abhishek.kp@dkv.global>
2023-12-26 12:01:41 +0530

    added gameplay page

--- src/pages/app/gameuser/introduction/index.jsx
+++ src/pages/app/gameuser/introduction/index.jsx
@@ -1,45 +1,64 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./intro.module.css";
 import Button from "../../../../components/common/button";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router";
 
 const Intro = () => {
   const videoRef = useRef(null);
 
+  const navigate = useNavigate();
+
+  const handlePlay = () => {
+    // videoRef.current.play().catch((error) => {});
+    navigate("/gameplay");
+  };
+
   useEffect(() => {
-    const playVideo = () => {
+    const handleEnded = () => {
+      // Video has ended, restart it
       videoRef.current.currentTime = 0; // Rewind to the beginning
       videoRef.current.play();
-      setTimeout(() => {
-        videoRef.current.muted = false;
-      }, 2000);
-    };
-
-    // Event listener to start the video when loaded
-    const handleLoadedData = () => {
-      playVideo();
+      videoRef.current.muted = false;
     };
 
     // Attach the event listener
-    videoRef.current.addEventListener("loadeddata", handleLoadedData);
+    videoRef.current.addEventListener("ended", handleEnded);
+
+    // Start the video
+    videoRef.current
+      .play()
+      .then(() => {})
+      .catch((error) => {
+        console.error("Autoplay failed:", error);
+      });
 
     return () => {
       // Cleanup: remove the event listener
-      videoRef.current.removeEventListener("loadeddata", handleLoadedData);
+      videoRef.current.removeEventListener("ended", handleEnded);
     };
   }, []);
-
   return (
-    <div className={styles.container}>
-      <div className={styles.videoContainer} controls>
-        <video ref={videoRef} width="100%" height="100%" controls>
-          <source src="./example.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <div className={styles.buttonContainer}>
-          <Button customStyle={{ fontSize: "1.4rem" }}>Skip</Button>
+    <motion.div
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -100 }}
+      transition={{ duration: 0.3, damping: 10 }}
+    >
+      <div className={styles.container}>
+        <div className={styles.videoContainer}>
+          <video ref={videoRef} width="100%" height="100%" controls={false}>
+            <source src="./example.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <div className={styles.buttonContainer}>
+            <Button onClick={handlePlay} customStyle={{ fontSize: "1.4rem" }}>
+              Skip
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/routers/user/index.jsx
+++ src/routers/user/index.jsx
@@ -6,6 +6,7 @@ import styles from "./userroutes.module.css";
 import Homepage from "../../pages/app/gameuser/homepage";
 import Profile from "../../pages/app/common/profile";
 import Intro from "../../pages/app/gameuser/introduction";
+import GamePlay from "../../pages/app/gameuser/gameplay";
 
 const User = () => {
   const location = useLocation();
@@ -20,6 +21,7 @@ const User = () => {
           <Route path="/" element={<Homepage />} />
           <Route path="/profile/:userID?" element={<Profile />} />
           <Route path="/intro" element={<Intro />} />
+          <Route path="/gameplay" element={<GamePlay />} />
         </Routes>
       </div>
     </div>
