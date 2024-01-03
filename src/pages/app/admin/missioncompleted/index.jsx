57fedc64b30f9fe1b5fa48fb389b8e3baa2fbf65
abhishek.kp <abhishek.kp@dkv.global>
2024-01-03 17:58:45 +0530

    changes

--- src/pages/app/admin/missioncompleted/index.jsx
+++ src/pages/app/admin/missioncompleted/index.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./missioncompleted.module.css";
 
 const MissionCompleted = () => {
-  return <div>MissionCompleted</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.missionContainer}>Mission Completed</div>
+    </div>
+  );
 };
 
 export default MissionCompleted;
