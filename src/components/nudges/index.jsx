8548c3852af285394e7874f72791fa8676bfc164
abhishek.kp <abhishek.kp@dkv.global>
2024-01-25 16:28:31 +0530

    changes

--- src/components/nudges/index.jsx
+++ src/components/nudges/index.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./nudges.module.css";
 import Button from "../common/button";
 
 const Nudges = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
+      <div
+        className={styles.header}
+        onClick={() => {
+          setShow((prev) => {
+            return !prev;
+          });
+        }}
+      >
         <div>Nudges</div>
-        <div className={styles.arrow}>
+        <div className={`${styles.arrow} ${show ? styles.show : ""}`}>
           <svg onClick={() => {}}>
             <use xlinkHref={"sprite.svg#up_arrow"} />
           </svg>
         </div>
       </div>
-      <div className={styles.description}>
-        <div className={styles.input}>
-          <textarea draggable={false}></textarea>
+      {show && (
+        <div className={styles.description}>
+          <div className={styles.input}>
+            <textarea draggable={false}></textarea>
+          </div>
+          <div className={styles.buttonContainer}>
+            <Button>Send</Button>
+          </div>
         </div>
-        <div className={styles.buttonContainer}>
-          <Button>Send</Button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/teammembers/index.jsx
+++ src/components/teammembers/index.jsx
@@ -1,48 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./teammembers.module.css";
 
 const TeamMembers = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
+      <div
+        className={styles.header}
+        onClick={() => {
+          setShow((prev) => {
+            return !prev;
+          });
+        }}
+      >
         <div>Team Members</div>
-        <div className={styles.arrow}>
+        <div className={`${styles.arrow} ${show ? styles.show : ""}`}>
           <svg onClick={() => {}}>
             <use xlinkHref={"sprite.svg#up_arrow"} />
           </svg>
         </div>
       </div>
-      <div className={styles.mainContainer}>
-        <div className={styles.item}>
-          <div>Admin</div>
-          <div className={styles.userContainer}>
-            <div className={styles.itemDetails}>
-              <div>A</div>
-              <div>Abhishek</div>
-              <div>CFO</div>
-              <div></div>
+      {show && (
+        <div className={styles.mainContainer}>
+          <div className={styles.item}>
+            <div>Admin</div>
+            <div className={styles.userContainer}>
+              <div className={styles.itemDetails}>
+                <div>A</div>
+                <div>Abhishek</div>
+                <div>CFO</div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.item}>
+            <div>CTO</div>
+            <div className={styles.userContainer}>
+              <div className={styles.itemDetails}>
+                <div>B</div>
+                <div>Bushan</div>
+                <div>CTO</div>
+                <div></div>
+              </div>
+              <hr />
+              <div className={styles.itemDetails}>
+                <div>C</div>
+                <div>Chandan</div>
+                <div>CTO</div>
+                <div></div>
+              </div>
             </div>
           </div>
         </div>
-        <div className={styles.item}>
-          <div>CTO</div>
-          <div className={styles.userContainer}>
-            <div className={styles.itemDetails}>
-              <div>B</div>
-              <div>Bushan</div>
-              <div>CTO</div>
-              <div></div>
-            </div>
-            <hr />
-            <div className={styles.itemDetails}>
-              <div>C</div>
-              <div>Chandan</div>
-              <div>CTO</div>
-              <div></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
