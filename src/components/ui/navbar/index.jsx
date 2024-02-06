5053ff2bcd1764dc4a950bfcfcadb0b2ae5acfef
abhishek.kp <abhishek.kp@dkv.global>
2024-02-06 12:59:33 +0530

    changes

--- src/components/ui/navbar/index.jsx
+++ src/components/ui/navbar/index.jsx
@@ -1,15 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./navbar.module.css";
 import logo from "../../../assets/logo/pwclabel.png";
-import { useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import ModalContainer from "../../modal";
+import { logoutUser } from "../../../store/auth/login";
+import Button from "../../common/button";
 
 const Navbar = ({ role = "Admin" }) => {
   const [initial, setInitials] = useState("");
 
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
+
+  const [showDropdown, setShowDropdown] = useState(false);
+
   const { credentials } = useSelector((state) => state.login);
 
+  const { isConnectedToServer } = useSelector((state) => state.gameplay);
+  const { sessionDetails } = useSelector((state) => state.getSession);
+
   const navigate = useNavigate();
+
+  const dropdownRef = useRef(null);
+  const dispatch = useDispatch();
+  const location = useLocation();
 
   const extractInitials = (username) => {
     const words = username.split(" ");
@@ -34,6 +48,23 @@ const Navbar = ({ role = "Admin" }) => {
     }
   }, []);
 
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showDropdown) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showDropdown]);
+
   return (
     <div className={styles.navbarContainer}>
       <Link to={"/"} className={styles.pwcLogo}>
@@ -45,12 +76,96 @@ const Navbar = ({ role = "Admin" }) => {
         <div
           className={styles.profileIcon}
           onClick={() => {
-            navigate(`/profile/${credentials.data.userID}`);
+            setShowDropdown(!showDropdown);
           }}
         >
           {initial}
         </div>
       </div>
+      {showDropdown && (
+        <div ref={dropdownRef} className={styles.dropdownContainer}>
+          <div className={styles.headRow}>
+            <div className={styles.playerDetails}>
+              {credentials?.data?.userName ? (
+                <div>{credentials.data.userName}</div>
+              ) : (
+                <div></div>
+              )}
+
+              {credentials?.data?.emailID ? (
+                <div>{credentials.data.emailID}</div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+            <div className={styles.profileIcon2}>{initial}</div>
+          </div>
+          <hr></hr>
+          <div
+            onClick={() => {
+              setShowDropdown(false);
+              navigate(`/profile/${credentials.data.userID}`);
+            }}
+            className={styles.row}
+          >
+            Edit Profile
+          </div>
+          <div
+            className={styles.row}
+            onClick={() => {
+              setShowDropdown(false);
+              setShowLogoutModal(true);
+            }}
+          >
+            Logout
+          </div>
+        </div>
+      )}
+
+      {showLogoutModal && (
+        <ModalContainer>
+          <div className={"modal_content"}>
+            <div className={"modal_header"}>
+              <div>Logout:</div>
+              <div>
+                <svg
+                  className="modal_crossIcon"
+                  onClick={() => {
+                    setShowLogoutModal(null);
+                  }}
+                >
+                  <use xlinkHref={"sprite.svg#crossIcon"} />
+                </svg>
+              </div>
+            </div>
+            <div className={"modal_description"}>
+              Are you sure you want to logout ?
+            </div>
+
+            <div className={"modal_buttonContainer"}>
+              <Button
+                buttonType={"cancel"}
+                onClick={() => {
+                  setShowLogoutModal(null);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                customStyle={{
+                  marginLeft: "1rem",
+                }}
+                onClick={() => {
+                  dispatch(logoutUser());
+                  navigate("/");
+                }}
+              >
+                Confirm
+              </Button>
+            </div>
+          </div>
+        </ModalContainer>
+      )}
     </div>
   );
 };
