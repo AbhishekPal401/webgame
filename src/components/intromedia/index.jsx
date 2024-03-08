961cb12a21f048f8d2709470f426c71183553ea7
abhishek.kp <abhishek.kp@dkv.global>
2024-03-08 11:56:04 +0530

    filestream for intromedia

--- src/components/intromedia/index.jsx
+++ src/components/intromedia/index.jsx
@@ -3,6 +3,9 @@ import styles from "./intromedia.module.css";
 import { extractFileType } from "../../utils/helper";
 import PDFPreview from "../preview/pdfpreview";
 import DOMPurify from "dompurify";
+import axios from "axios";
+import { baseUrl } from "../../middleware/url";
+import QuestionLoader from "../loader/questionLoader";
 
 const IntroMedia = ({
   onCancel = () => {},
@@ -11,6 +14,39 @@ const IntroMedia = ({
   mediaType = "",
 }) => {
   const [resourceType, setResourceType] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [fileStream, setFileStream] = useState(null);
+
+  const getFileStream = async (url) => {
+    setLoading(true);
+
+    const data = {
+      fileName: url,
+      module: "Scenario",
+    };
+
+    const headers = { "Content-Type": "application/json" };
+
+    const response = await axios.request({
+      baseURL: baseUrl,
+      url: "/api/Storage/GetFileStream",
+      method: "POST",
+      data: data,
+      headers,
+      responseType: "blob",
+    });
+    if (response.data) {
+      setFileStream(URL.createObjectURL(new Blob([response.data])));
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (mediaURL) {
+      getFileStream(mediaURL);
+    }
+  }, [mediaURL]);
 
   useEffect(() => {
     if (mediaURL) {
@@ -65,30 +101,36 @@ const IntroMedia = ({
         )}
       </div>
 
-      <div style={{ height: "72vh" }}>
-        {resourceType === "Video" ? (
-          <video controls className={styles.video}>
-            <source src={mediaURL} />
-            Your browser does not support the video tag.
-          </video>
-        ) : resourceType === "Audio" ? (
-          <div className={styles.audio}>
-            <img src="./images/audio_background.png" alt="Audio Background" />
-            <audio controls controlsList="nodownload">
-              <source src={mediaURL} />
-              Your browser does not support the audio tag.
-            </audio>
-          </div>
-        ) : resourceType === "Pdf" ? (
-          <PDFPreview pdfUrl={mediaURL} customStyles={styles.pdf} />
-        ) : resourceType === "Image" ? (
-          <img
-            src={mediaURL}
-            alt="resource Image"
-            className={styles.previewImage}
-          />
-        ) : null}
-      </div>
+      {loading ? (
+        <div style={{ height: "50vh" }}>
+          <QuestionLoader />
+        </div>
+      ) : (
+        <div style={{ height: "72vh" }}>
+          {resourceType === "Video" && fileStream ? (
+            <video controls className={styles.video}>
+              <source src={fileStream} />
+              Your browser does not support the video tag.
+            </video>
+          ) : resourceType === "Audio" && fileStream ? (
+            <div className={styles.audio}>
+              <img src="./images/audio_background.png" alt="Audio Background" />
+              <audio controls controlsList="nodownload">
+                <source src={fileStream} />
+                Your browser does not support the audio tag.
+              </audio>
+            </div>
+          ) : resourceType === "Pdf" && fileStream ? (
+            <PDFPreview pdfUrl={fileStream} customStyles={styles.pdf} />
+          ) : resourceType === "Image" && fileStream ? (
+            <img
+              src={fileStream}
+              alt="resource Image"
+              className={styles.previewImage}
+            />
+          ) : null}
+        </div>
+      )}
     </div>
   );
 };
