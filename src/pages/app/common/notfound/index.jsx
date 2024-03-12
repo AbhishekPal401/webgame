3515e73e20ee7a95639a7dd4ef2c3db8a51ecca3
abhishek.kp <abhishek.kp@dkv.global>
2024-03-12 12:19:45 +0530

    added naviaget to home page is not authorised page

--- src/pages/app/common/notfound/index.jsx
+++ src/pages/app/common/notfound/index.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./notfound.module.css";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, []);
+
   return <div>NotFound</div>;
 };
 
