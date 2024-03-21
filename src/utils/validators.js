b6b5275c10b538453c1aaed95b5c2e5df3469b5c
Rushibhawar <rushikeshbhawar1213@gmail.com>
2024-03-21 16:44:44 +0530

    QA [ error messages  mistakes] resolved

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -9,14 +9,14 @@ export const validatePassword = (password) => {
   return re.test(password);
 };
 
-  export const validateEmail = (email) => {
-    // const re =
-    //   /[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-    // const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // email changes min 6 
-    // const re = /^[a-zA-Z0-9._-]{6,254}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // email changes min 6 and max 254 characters
-    const re = /^(?=.{6,254}$)[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // email changes with max 254 and min 6 chars
-    return re.test(email);
-  };
+export const validateEmail = (email) => {
+  // const re =
+  //   /[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  // const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // email changes min 6 
+  // const re = /^[a-zA-Z0-9._-]{6,254}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // email changes min 6 and max 254 characters
+  const re = /^(?=.{6,320}$)[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // email changes with max 320 and min 6 chars
+  return re.test(email);
+};
 
 export const validatePhone = (phone) => {
   const re = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/; // only starting with 6789
