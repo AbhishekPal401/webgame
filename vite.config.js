12051f6901a2e3d939f086600dfce1a25ba75e52
abhishek.kp <abhishek.kp@dkv.global>
2024-03-28 13:38:29 +0530

    added console.log remover

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 import dotenv from "dotenv";
+import removeConsole from "vite-plugin-remove-console";
 
 dotenv.config(); // Load environment variables
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeConsole()],
   // server: {
   //   port: 8080,
   // },
